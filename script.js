34c37b8cc39dd0d497e4bc82b0b174f3fd8de2e0
LucasRocha <lucaspdsr@gmail.com>
2024-03-19 22:45:11 -0300

    Adicionada transição suave para a seção "acertos"
    Este commit inclui alterações para adicionar uma transição suave à seção "acertos". As alterações incluem:
    
    - Alteração da propriedade CSS 'display' para 'visibility' na seção "acertos" para permitir a transição de opacidade.
    - Adicionado um ouvinte de evento JavaScript ao botão "Finalizar" que altera a visibilidade e a opacidade da seção "acertos" quando o botão é clicado.
    
    mostrando a seção "acertos" de forma suave e gradual.

--- script.js
+++ script.js
@@ -99,7 +99,12 @@ const corretas = new Set() // Cria um conjunto vazio e armazena na variável "co
 const totalDePerguntas = perguntas.length // Armazena o tamanho do array "perguntas" na variável "totalDePerguntas"
 const mostrarResultado = document.querySelector('#acertos span') // Define a função "mostrarResultado"
 
-
+// Evento de clique
+document.getElementById('finalizar').addEventListener('click', function() {
+    var acertos = document.getElementById('acertos');
+    acertos.style.visibility = 'visible';
+    acertos.style.opacity = '1';
+});
 
 // Laço de repetição
 for(const item of perguntas) { // Para cada item no array "perguntas"
